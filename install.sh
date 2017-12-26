58316812fd81304c24bedcb9ad6a41f7d2fdd987
joshtt <joshtt24@gmail.com>
2017-12-25 19:24:43 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+files="config i3blocks xinitrc Xresources"    # list of files/folders to symlink in homedir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
